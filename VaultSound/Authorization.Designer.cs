baf5bc4e1552289666c4a5243c676b44faa94afb
Artoym <artemaaawer@gmail.com>
2024-09-28 08:58:15 +0400

    ver1

--- VaultSound/Authorization.Designer.cs
+++ VaultSound/Authorization.Designer.cs
@@ -49,11 +49,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label2.Location = new System.Drawing.Point(405, 9);
+            this.label2.Location = new System.Drawing.Point(282, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(158, 28);
             this.label2.TabIndex = 12;
@@ -62,11 +59,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Location = new System.Drawing.Point(12, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 28);
             this.label1.TabIndex = 13;
@@ -77,16 +71,13 @@
             this.Nick.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Nick.Location = new System.Drawing.Point(17, 154);
             this.Nick.Name = "Nick";
-            this.Nick.Size = new System.Drawing.Size(245, 35);
+            this.Nick.Size = new System.Drawing.Size(213, 35);
             this.Nick.TabIndex = 14;
             // 
             // Mail
             // 
             this.Mail.AutoSize = true;
-            this.Mail.BackColor = System.Drawing.Color.Transparent;
-            this.Mail.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Mail.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Mail.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Mail.Location = new System.Drawing.Point(13, 129);
             this.Mail.Name = "Mail";
             this.Mail.Size = new System.Drawing.Size(88, 22);
@@ -96,10 +87,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label3.Location = new System.Drawing.Point(13, 192);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 22);
@@ -111,17 +99,14 @@
             this.Pass.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Pass.Location = new System.Drawing.Point(17, 217);
             this.Pass.Name = "Pass";
-            this.Pass.Size = new System.Drawing.Size(245, 35);
+            this.Pass.Size = new System.Drawing.Size(213, 35);
             this.Pass.TabIndex = 17;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label4.Location = new System.Drawing.Point(766, 51);
+            this.label4.Location = new System.Drawing.Point(591, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(150, 28);
             this.label4.TabIndex = 18;
@@ -130,11 +115,8 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label5.Location = new System.Drawing.Point(658, 129);
+            this.label5.Location = new System.Drawing.Point(561, 129);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(196, 22);
             this.label5.TabIndex = 19;
@@ -143,19 +125,16 @@
             // newNick
             // 
             this.newNick.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.newNick.Location = new System.Drawing.Point(662, 154);
+            this.newNick.Location = new System.Drawing.Point(565, 154);
             this.newNick.Name = "newNick";
-            this.newNick.Size = new System.Drawing.Size(254, 35);
+            this.newNick.Size = new System.Drawing.Size(192, 35);
             this.newNick.TabIndex = 20;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label6.Location = new System.Drawing.Point(658, 192);
+            this.label6.Location = new System.Drawing.Point(561, 205);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(182, 22);
             this.label6.TabIndex = 21;
@@ -164,19 +143,16 @@
             // newPass
             // 
             this.newPass.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.newPass.Location = new System.Drawing.Point(662, 215);
+            this.newPass.Location = new System.Drawing.Point(565, 230);
             this.newPass.Name = "newPass";
-            this.newPass.Size = new System.Drawing.Size(254, 35);
+            this.newPass.Size = new System.Drawing.Size(192, 35);
             this.newPass.TabIndex = 22;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label7.Location = new System.Drawing.Point(658, 253);
+            this.label7.Location = new System.Drawing.Point(561, 268);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(85, 22);
             this.label7.TabIndex = 23;
@@ -185,9 +161,9 @@
             // Mail0
             // 
             this.Mail0.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Mail0.Location = new System.Drawing.Point(662, 278);
+            this.Mail0.Location = new System.Drawing.Point(565, 293);
             this.Mail0.Name = "Mail0";
-            this.Mail0.Size = new System.Drawing.Size(254, 35);
+            this.Mail0.Size = new System.Drawing.Size(192, 35);
             this.Mail0.TabIndex = 24;
             // 
             // Enter
@@ -203,7 +179,7 @@
             // Reg
             // 
             this.Reg.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Reg.Location = new System.Drawing.Point(751, 319);
+            this.Reg.Location = new System.Drawing.Point(592, 334);
             this.Reg.Name = "Reg";
             this.Reg.Size = new System.Drawing.Size(165, 39);
             this.Reg.TabIndex = 26;
@@ -213,7 +189,7 @@
             // sw2
             // 
             this.sw2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.sw2.Location = new System.Drawing.Point(410, 530);
+            this.sw2.Location = new System.Drawing.Point(306, 530);
             this.sw2.Name = "sw2";
             this.sw2.Size = new System.Drawing.Size(165, 39);
             this.sw2.TabIndex = 27;
@@ -226,8 +202,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackgroundImage = global::VaultSound.Properties.Resources.Rectangle_1;
-            this.ClientSize = new System.Drawing.Size(928, 581);
+            this.ClientSize = new System.Drawing.Size(769, 581);
             this.Controls.Add(this.sw2);
             this.Controls.Add(this.Reg);
             this.Controls.Add(this.Enter);
